a1ed42c9b75264fdcd5f5e52f264299cda220ff0
森祐一朗 <apotikisin4632@gmail.com>
2024-04-08 09:57:03 +0900

    コメントをもとにLoginModalを表示する実装を変更

--- src/components/modals/LoginModal.jsx
+++ src/components/modals/LoginModal.jsx
@@ -10,29 +10,25 @@ import { useLoginHook } from "../../hooks/loginModal/useLoginHook";
 export const LoginModal = memo(({ showLoginModal, onClickShowLoginModal }) => {
     const { loginAccount, errorMsgs, onChangeLoginAccount, onClickLogin } = useLoginHook();
 
+    if (!showLoginModal) return <></>
     return (
-        <>
-            {
-                showLoginModal && (
-                    <div className="overlay">
-                        <div className="modal">
-                            <div className="header">
-                                <IoArrowBack onClick={onClickShowLoginModal} />
-                                <h1>ログイン</h1>
-                            </div>
-                            {errorMsgs && errorMsgs.map((errorMsg, index
-                            ) => (
-                                <p key={index}>{errorMsg}</p>
-                            )
-                            )}
-                            <form className="login-form" action="">
-                                <InputField type='text' name='email' value={loginAccount.email} onChange={onChangeLoginAccount} placeholder='Eメール' />
-                                <InputField type='password' name='password' value={loginAccount.password} onChange={onChangeLoginAccount} placeholder='パスワード' />
-                                <Button onClick={onClickLogin}>ログイン</Button>
-                            </form>
-                        </div>
-                    </div>
+        <div className="overlay">
+            <div className="modal">
+                <div className="header">
+                    <IoArrowBack onClick={onClickShowLoginModal} />
+                    <h1>ログイン</h1>
+                </div>
+                {errorMsgs && errorMsgs.map((errorMsg, index
+                ) => (
+                    <p key={index}>{errorMsg}</p>
+                )
                 )}
-        </>
+                <form className="login-form" action="">
+                    <InputField type='text' name='email' value={loginAccount.email} onChange={onChangeLoginAccount} placeholder='Eメール' />
+                    <InputField type='password' name='password' value={loginAccount.password} onChange={onChangeLoginAccount} placeholder='パスワード' />
+                    <Button onClick={onClickLogin}>ログイン</Button>
+                </form>
+            </div>
+        </div>
     );
 })
