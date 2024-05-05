40d96cf3575c720a6538a261282112f19d9ff41c
森祐一朗 <apotikisin4632@gmail.com>
2024-05-05 14:47:42 +0900

    ツイート削除機能の作成

--- src/components/atoms/Tweet.jsx
+++ src/components/atoms/Tweet.jsx
@@ -2,15 +2,27 @@ import { HiOutlineChatBubbleLeft } from "react-icons/hi2";
 import { AiOutlineRetweet } from "react-icons/ai";
 import { CiHeart } from "react-icons/ci";
 import { CiBookmark } from "react-icons/ci";
+import { HiDotsHorizontal } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import "../../style/atoms/tweet/Tweet.scss"
+import { TweetHook } from "../../hooks/atoms/TweetHook";
 
 export const Tweet = ({ tweet, user }) => {
+    const {
+        currentUserData,
+        regexProfile,
+        location,
+        deleteTweet
+    } = TweetHook(tweet);
+
     return (
         <>
-            <Link to={`/profile/${user.id}`}>
-                <p>{user.name}</p>
-            </Link>
+            <div className="tweet-name-dots">
+                <Link to={`/profile/${user.id}`}>
+                    <p>{user.name}</p>
+                </Link>
+                {(regexProfile.test(location.pathname) && currentUserData.data.id === user.id) && <HiDotsHorizontal onClick={deleteTweet} />}
+            </div>
             <Link to={`/tweets/${tweet.id}`}>
                 <p className="tweet-text">{tweet.content}</p>
                 <div className="images">
